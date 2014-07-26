98dedc7cb8997ad07da6cd39608661cc7e671ce0
Charles Macanka <cmacanka@gmail.com>
2014-07-26 18:00:02 -0400

    Added trivial console and library projects.

---
+++ LootGainConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using LootGainLib.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LootGainConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                System.Console.WriteLine("Must provide a file name as an argument.");
+                System.Console.ReadLine();
+                return;
+            }
+
+            var parser = new FileParser();
+            var sources = parser.Parse(args[0]);
+
+            System.Console.WriteLine("Done.  Parsed {0} data sources.", sources.Count);
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ LootGainLib/Parsers/FileParser.cs
@@ -0,0 +1,22 @@
+﻿using LootGainLib.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LootGainLib.Parsers
+{
+    public class FileParser
+    {
+        public FileParser()
+        {
+
+        }
+
+        public List<DataSource> Parse(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
